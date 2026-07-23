ad6246aefd6daaad47903ba1e34b0f11578a647c
Ankit Jaiswal <73015722+AnkitJaiswal97@users.noreply.github.com>
2026-07-23 22:10:52 +0530

    Update build.gradle.kts for Java 17 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
     namespace = "com.shivay.medicalinventory"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.shivay.medicalinventory"
         minSdk = 24
@@ -13,13 +15,30 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    buildFeatures { compose = true }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        compose = true
+    }
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.compose.material3:material3")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
